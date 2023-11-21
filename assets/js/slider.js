ee95cb0a0f640e12b62d41304feab3dd91866509
gorkasl <gsanchez@diariovasco.com>
2023-11-21 20:42:42 +0100

    dia21

---
+++ assets/js/slider.js
@@ -0,0 +1,22 @@
+/* DECLARACIÓN DE VARIABLES */
+
+var numfotos = 12; /* nÚmero total de fotos */
+var ordenprincipal, ordesiguiente;
+var intervalo, temporizador;
+var tiempoespera = 500;
+
+const flechaIzd = document.getElementById("flechaIzd");
+const flechaDer = document.getElementById("flechaDer");
+const fotoActiva = document.getElementById("fotoActiva");
+
+/* ACCIONES DE LAS FLECHAS AL PULSAR EN ELLAS O CÓDIGO PARA LA ACCIÓN DE LAS FLECHAS*/
+
+/* LISTENER DEL CLICK EN LA FLECHA IZQUIERDA */
+
+flechaIzd.addEventListener("click", function(){
+   
+    /* Recogemos el valor, escuchamos el click de la imagen flechaIzd */
+    
+
+});
+
