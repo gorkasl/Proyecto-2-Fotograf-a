4707245990d65bc6130c60167f7bb364b9643c9b
gorkasl <gsanchez@diariovasco.com>
2023-11-22 10:10:47 +0100

    copia_amaia

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,22 +1,39 @@
-/* DECLARACIÓN DE VARIABLES */
+/* DECLARACION DE VARIABLES */
 
-var numfotos = 12; /* nÚmero total de fotos */
-var ordenprincipal, ordesiguiente;
+var numFotos = 12; /* El slider tendrá 12 fotos */
+var ordenPrincipal, ordenSiguiente;
 var intervalo, temporizador;
-var tiempoespera = 500;
+var tiempoEspera = 5000; /* (milisegundos = 5 segundos) */
+
+/* DECLARACION DE CONSTANTES */
 
 const flechaIzd = document.getElementById("flechaIzd");
+/* console.log(flechaIzd); */
 const flechaDer = document.getElementById("flechaDer");
+/* console.log(flechaDer); */
 const fotoActiva = document.getElementById("fotoActiva");
+/* console.log(fotoActiva) */
 
-/* ACCIONES DE LAS FLECHAS AL PULSAR EN ELLAS O CÓDIGO PARA LA ACCIÓN DE LAS FLECHAS*/
+/* ACCION DE FLECHAS AL PULSAR, PASAR ADELANTE O ATRAS EN INFINITO */
 
-/* LISTENER DEL CLICK EN LA FLECHA IZQUIERDA */
-
+/* ESCUCHAMOS CLICK EN ELEMENTO FLECHA IZD */
 flechaIzd.addEventListener("click", function(){
-   
-    /* Recogemos el valor, escuchamosa el click de la imagen flechaIzd */
-    
 
-});
+    /* Recogemos el valor String del atributo orden del elemento img con la foto principal*/
+    ordenPrincipal = fotoActiva.getAttribute("orden");
+    ordenPrincipal = Number(ordenPrincipal);
+
+    /*condición para controlar que no se salga del total de fotos  */
+    if(ordenPrincipal === 1){
+        ordenSiguiente = numFotos;
+    }else{
+        ordenSiguiente = ordenPrincipal - 1;
+    }
+
+    /* fotoActiva.src="./assets/img/slider1_2560.jpg" */
+    /* fotoActiva.src="./assets/img/slider"+ordenSiguiente+"_2560.jpg"; */
+    fotoActiva.src=`./assets/img/slider${ordenSiguiente}_2560.jpg`;
+    fotoActiva.setAttribute("orden", ordenSiguiente);
+   
+    })
 
