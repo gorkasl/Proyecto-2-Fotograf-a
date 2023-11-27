fac3c628b2fa55a8bfb583c06dbe2e9977f706cd
gorkasl <gsanchez@diariovasco.com>
2023-11-27 17:45:23 +0100

    todo

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -32,8 +32,29 @@ flechaIzd.addEventListener("click", function(){
 
     /* fotoActiva.src="./assets/img/slider1_2560.jpg" */
     /* fotoActiva.src="./assets/img/slider"+ordenSiguiente+"_2560.jpg"; */
-    fotoActiva.src=`./assets/img/slider${ordenSiguiente}_2560.jpg`;
+    fotoActiva.src=`../assets/img/slider${ordenSiguiente}_2560.jpg`;
     fotoActiva.setAttribute("orden", ordenSiguiente);
    
     })
 
+/* ESCUCHAMOS CLICK EN ELEMENTO FLECHA DER */
+
+flechaDer.addEventListener("click", function(){
+
+    /* Recogemos el valor String del atributo orden del elemento img con la foto principal*/
+    ordenPrincipal = fotoActiva.getAttribute("orden");
+    ordenPrincipal = Number(ordenPrincipal);
+
+    /*condición para controlar que no se salga del total de fotos  */
+    if(ordenPrincipal === 1){
+        ordenSiguiente = numFotos;
+    }else{
+        ordenSiguiente = ordenPrincipal + 1;
+    }
+
+    /* fotoActiva.src="./assets/img/slider1_2560.jpg" */
+    /* fotoActiva.src="./assets/img/slider"+ordenSiguiente+"_2560.jpg"; */
+    fotoActiva.src=`../assets/img/slider${ordenSiguiente}_2560.jpg`;
+    fotoActiva.setAttribute("orden", ordenSiguiente);
+   
+    })
