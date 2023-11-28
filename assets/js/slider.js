584a838416950cb5edad690feafa9badb300dabb
gorkasl <gsanchez@diariovasco.com>
2023-11-28 20:30:41 +0100

    dia_28-11 javascript fotos

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -5,6 +5,7 @@ var ordenPrincipal, ordenSiguiente;
 var intervalo, temporizador;
 var tiempoEspera = 5000; /* (milisegundos = 5 segundos) */
 
+
 /* DECLARACION DE CONSTANTES */
 
 const flechaIzd = document.getElementById("flechaIzd");
@@ -34,10 +35,13 @@ flechaIzd.addEventListener("click", function(){
     /* fotoActiva.src="./assets/img/slider"+ordenSiguiente+"_2560.jpg"; */
     fotoActiva.src=`./assets/img/slider${ordenSiguiente}_2560.jpg`;
     fotoActiva.setAttribute("orden", ordenSiguiente);
-   
+
+    clearInterval(intervalo); /* para el intervalo que está cambiado la foto cada 5 segundos */
+    crearIntervalo(); /* Para volver a crear el intervalo */
+  
     })
 
-/* ESCUCHAMOS CLICK EN ELEMENTO FLECHA DER */
+/* ESCUCHAMOS CLICK EN ELEMENTO FLECHA DER / EVENTO DE ESCUCHA*/
 
 flechaDer.addEventListener("click", function(){
 
@@ -56,5 +60,35 @@ flechaDer.addEventListener("click", function(){
     /* fotoActiva.src="./assets/img/slider"+ordenSiguiente+"_2560.jpg"; */
     fotoActiva.src=`./assets/img/slider${ordenSiguiente}_2560.jpg`;
     fotoActiva.setAttribute("orden", ordenSiguiente);
-   
+
+    clearInterval(intervalo); /* para el intervalo que está cambiado la foto cada 5 segundos */
+    crearIntervalo(); /* Para volver a crear el intervalo */
     })
+
+/* METER FOTO RANMDOM / ZONA DE IMAGEN RANDOM CADA VEZ QUE CARGA LA PÁGINA WEB  */
+/* Lo metemos en la declaración de funciones abajo*/
+ funcion_random(); /* Hace la llamada a la función de abajo */
+
+/* CAMBIAR FOTO PRINCIPAL EN INTERVALOS DE TIEMPO */
+
+/* crearIntervalo(tiempoEspera); */
+crearIntervalo();/* Hace la llamada a la funcion de abajo */
+
+
+/* DECLARACIÓN FUNCIONES */
+
+/* Función para cambiar la foto de forma random */
+function funcion_random(){
+    let numAleatorio;
+    numAleatorio = Number(numAleatorio);
+    numAleatorio = Math.random()*numFotos; /* Nos da un número con decimales */
+    numAleatorio = Math.ceil(numAleatorio); /* Convierte el número a entero */
+    fotoActiva.src=`./assets/img/slider${numAleatorio}_2560.jpg`;/* cambiamos el src */
+    fotoActiva.setAttribute("orden", numAleatorio); /* cambiamos el atributo orden de fotoActiva */
+}
+
+/* Función crear intervalo */
+function crearIntervalo(valorRecibido){
+    intervalo = window.setInterval(funcion_random, 5000); /* Cambia da 5 segundos */
+
+}
