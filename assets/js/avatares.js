2e9bbb0c92c0237a62c91bb80810b6fbbc22d32c
gorkasl <gsanchez@diariovasco.com>
2023-12-05 19:45:51 +0100

    js-objetibo-dia5

--- assets/js/avatares.js
+++ assets/js/avatares.js
@@ -1,14 +1,14 @@
 /* Recogemos en una constante (array) todos los elementos del html que tenga como clase imgObjetivo */
-const objetivos = document.getElementsByClassName("imgObjetivo");
+const imgObjetivos = document.getElementsByClassName("imgObjetivo");
 
 /* Hacemos una iteración para recorrer cada item de la constante, y escuchamos si se hace evento mouseover o mouseout sobre alguno de ellos */
 
-for(const objetivo of objetivos){
+for(const imgObjetivo of imgObjetivos){
     /* Código que se ejecuta en cada vuelta */
-    objetivo.addEventListener("mouseover", function(){
-        objetivo.style.transform="rotate(20deg)";
+    imgObjetivo.addEventListener("mouseover", function(){
+        imgObjetivo.style.transform="rotate(20deg)";
     })
-    objetivo.addEventListener("mouseout", function(){
-        objetivo.style.transform="rotate(0deg)";
+    imgObjetivo.addEventListener("mouseout", function(){
+        imgObjetivo.style.transform="rotate(0deg)";
     })
 }
