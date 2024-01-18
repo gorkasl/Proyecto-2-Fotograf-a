51543f8c639d9f405e7c96ecf5230f0069a2e167
gorkasl <gsanchez@diariovasco.com>
2024-01-18 18:26:39 +0100

    cambios3forms

--- php/enviar.php
+++ php/enviar.php
@@ -1,71 +1,17 @@
 <?php
 
-// INSTANCIAMOS RECURSO
-require_once "./includes/_config.php";
-require_once "./class/_comprobaciones.php";
-
-//Creamos la variable
-$comprobacion = new clase_comprobaciones;
 
 if(isset($_POST)){
-/* 1. Recoger datos y comprobar */
+/* Recoger datos y comprobar */
     $destino = "gsanchez@diariovasco.com"; /* CORREO DEL ADMIN */
     $nombre = $_POST ["nombre"]; /* Nombre del usuario */
-    $correo = $_POST ["correo"];
-    $telefono = $_POST ["telefono"];
-    $mensaje = $_POST ["mensaje"];
+    $correo = $_POST ["correo"]; /* Correo del usuario */
+    $telefono = $_POST ["telefono"]; /* Telefono del usuario */
+    $mensaje = $_POST ["mensaje"]; /* Mensaje del usuario */
 
-     // comprobamos que el nombre no venga vacío
-     if($comprobacion->comprobarVacio($nombre)){    
-        header("location:../index.html?fallo=1#hitoContacto");
-        die; //salimos de este PHP aquí, sin ejecutar el resto de líneas posteriores.
-    }
+     
 
-     // limpiamos de cualquier caracter usado en scripts maliciosos
-     $comprobacion->filtrarValorLight($nombre);
-
- // VALIDACIONES DE CORREO
-    // comprobamos que el correo no venga vacío
-    if($comprobacion->comprobarVacio($correo)){    
-        header("location:../index.html?fallo=2#hitoContacto");
-        die;
-    }
-    // limpiamos de cualquier caracter usado en scripts maliciosos
-    $comprobacion->filtrarValorLight($correo);
-    // comprobamos que el correo sea un correo
-    // llamamos a la función de la clase comprobaciones
-    if(!$comprobacion->validar_email($correo)){    
-        header("location:../index.html?fallo=3#hitoContacto");
-        die;
-    }
-
-    // VALIDACIONES DE TELÉFONO
-    // comprobamos que el teléfono no venga vacío
-    if($comprobacion->comprobarVacio($telefono)){    
-        header("location:../index.html?fallo=4#hitoContacto");
-        die;
-    }
-        // limpiamos de cualquier caracter usado en scripts maliciosos y quitamos espacios al número
-        $comprobacion->filtrarValor($telefono);
-        // comprobamos si es número
-    if(!$comprobacion->validar_numero($telefono)){
-            header("location:../index.html?fallo=5#hitoContacto");
-            die;
-    }
-
-        // COMPROBACIONES MENSAJE
-        // comprobamos que el nombre no venga vacío
-        if($comprobacion->comprobarVacio($mensaje)){    
-            header("location:../index.html?fallo=6#hitoContacto");
-            die;
-        }
-        // limpiamos de cualquier caracter usado en scripts maliciosos
-        $comprobacion->filtrarValorLight($mensaje);
-    
-    
-
-
-    /* datos que cogemos de tu cliente */
+    /* datos que cogemos del usuario */
     /* IP */
     $ip=$_SERVER['REMOTE_ADDR'];
 
